48ddf67aeba8cc8fc68bb070027992a99859b01d
John Rappazzo <j360thunder@hotmail.com>
2025-05-19 20:29:54 -0400

    Add some missing documentation

--- CartonCaps.Core/Services/DeferredLinking/IDeepLinkClient.cs
+++ CartonCaps.Core/Services/DeferredLinking/IDeepLinkClient.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace CartonCaps.Core.Services.DeferredDeepLinking
 {
+    /// <summary>
+    /// A client to interact with a third party deferred deep link service
+    /// </summary>
     public interface IDeepLinkClient
     {
+        /// <summary>
+        /// Sends a request to the service to create a deferred deep link with associated metadata
+        /// </summary>
+        /// <param name="destination">The destination the app should navigate to when resolving this deep link</param>
+        /// <param name="metaData">The data associated with this deep link, to be retrieved when resolved</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<string> CreateDeepLink(string destination, Dictionary<string, object> metaData, CancellationToken cancellationToken);
+        
+        /// <summary>
+        /// Fetches a deferred deep link from the service along with any associated metadata
+        /// </summary>
+        /// <param name="deferredLinkCode">The unique code that identifies the deferred link</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<(bool linkFound, DeferredLink? link)> FetchDeferredLink(string deferredLinkCode, CancellationToken cancellationToken);
     }
 }
